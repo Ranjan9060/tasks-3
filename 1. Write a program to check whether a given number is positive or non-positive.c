d2e90ee1777d642bf6005d668790e8e630005e4e
Ranjan Yadav <108995846+Ranjan9060@users.noreply.github.com>
2022-10-08 03:52:45 -0400

    Add files via upload

---
+++ 1. Write a program to check whether a given number is positive or non-positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a  Number:");
+
+    scanf("%d",&num);
+
+    if(num>0)
+  {
+    printf("Positive Number");
+  }
+    else
+  {
+    printf("Non-Positive Number");
+  }
+    return 0;
+}
